fd831f1bfe8515debc18c7e4121e9a73ff0fd9e8
Viktor Persson <viktor.persson@arcsin.se>
2021-09-15 17:07:19 +0200

    Update color test.

--- colortest.sh
+++ colortest.sh
@@ -13,8 +13,8 @@ text='test'
 fgs=('m' '1m' '30m' '1;30m' '31m' '1;31m' '32m' '1;32m' '33m' '1;33m' '34m' '1;34m' '35m' '1;35m' '36m' '1;36m' '37m' '1;37m')
 bgs=('40m' '41m' '42m' '43m' '44m' '45m' '46m' '47m')
 
-padding_size=2
-separator_size=2
+padding_size=1
+separator_size=1
 
 # script
 pad_str=$(printf "%${padding_size}s" "")
