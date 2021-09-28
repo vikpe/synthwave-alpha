90915eeac0cc01ee7019ec783b1234cbbe1463dd
Viktor Persson <viktor.persson@arcsin.se>
2021-09-28 14:57:26 +0200

    Update fzf colors.

--- README.md
+++ README.md
@@ -88,8 +88,8 @@ set -U fish_pager_color_secondary_description 7f7094 # description of every seco
 export FZF_DEFAULT_OPTS='
 --color=fg:#7f7094,bg:#241b30,hl:#b8b1bb
 --color=fg+:#0be6a6,bg+:#191221,hl+:#0be6a6
---color=info:#55a7fb,prompt:#0be6a6,pointer:#ff00f6
---color=marker:#0be6a6,spinner:#55a7fa,header:#00fbfd
+--color=info:#aa53f8,prompt:#0be6a6,pointer:#ff00f6
+--color=marker:#0be6a6,spinner:#aa53f8,header:#00fbfd
 '
 
 ```
--- tools/templates/fzf.sh
+++ tools/templates/fzf.sh
@@ -1,6 +1,6 @@
 export FZF_DEFAULT_OPTS='
---color=fg:#{WHITE_DARKER.hex},bg:#{BLACK.hex},hl:#{WHITE_DARK.hex}
---color=fg+:#{GREEN.hex},bg+:#{BLACK_DARK.hex},hl+:#{GREEN.hex}
---color=info:#55a7fb,prompt:#{GREEN.hex},pointer:#{MAGENTA.hex}
---color=marker:#{GREEN.hex},spinner:#{BLUE.hex},header:#{CYAN.hex}
+--color=fg:#7f7094,bg:#241b30,hl:#b8b1bb
+--color=fg+:#0be6a6,bg+:#191221,hl+:#0be6a6
+--color=info:#aa53f8,prompt:#0be6a6,pointer:#ff00f6
+--color=marker:#0be6a6,spinner:#aa53f8,header:#00fbfd
 '
