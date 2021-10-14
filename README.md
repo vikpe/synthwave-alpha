9547f5e70ebf03bd0b21363fbf43d2e6477043e9
Viktor Persson <viktor.persson@arcsin.se>
2021-10-14 17:15:11 +0200

    Update fish template.

--- README.md
+++ README.md
@@ -36,19 +36,19 @@ C | HEX | C | HEX | C | HEX | C | HEX
 ```sh
 # https://fishshell.com/docs/current/cmds/set_color.html#cmd-set-color
 
-set -U fish_color_normal WHITE # default color
-set -U fish_color_command BRMAGENTA # commands like echo
-set -U fish_color_keyword BRMAGENTA # keywords like if - this falls back on the command color if unset
-set -U fish_color_quote BRCYAN # quoted text like "abc"
-set -U fish_color_redirection BRYELLOW # IO redirections like >/dev/null
-set -U fish_color_end BRYELLOW # process separators like ';' and '&'
-set -U fish_color_error WHITE # syntax errors
-set -U fish_color_param BRWHITE # ordinary command parameters
-set -U fish_color_comment BRBLACK # comments like '# important'
+set -U fish_color_normal b9b1bc # default color
+set -U fish_color_command ff00f6 # commands like echo
+set -U fish_color_keyword ff00f6 # keywords like if - this falls back on the command color if unset
+set -U fish_color_quote 00fbfd # quoted text like "abc"
+set -U fish_color_redirection f9f972 # IO redirections like >/dev/null
+set -U fish_color_end f9f972 # process separators like ';' and '&'
+set -U fish_color_error b9b1bc # syntax errors
+set -U fish_color_param f2f2e3 # ordinary command parameters
+set -U fish_color_comment 7f7094 # comments like '# important'
 set -U fish_color_selection normal # selected text in vi visual mode
-set -U fish_color_operator BRYELLOW # parameter expansion operators like '*' and '~'
-set -U fish_color_escape CYAN # character escapes like 'n' and 'x70'
-set -U fish_color_autosuggestion BRBLACK # autosuggestions (the proposed rest of a command)
+set -U fish_color_operator f9f972 # parameter expansion operators like '*' and '~'
+set -U fish_color_escape 00b0b1 # character escapes like 'n' and 'x70'
+set -U fish_color_autosuggestion 7f7094 # autosuggestions (the proposed rest of a command)
 set -U fish_color_cwd normal # the current working directory in the default prompt
 set -U fish_color_user normal # the username in the default prompt
 set -U fish_color_host normal # the hostname in the default prompt
@@ -57,17 +57,17 @@ set -U fish_color_cancel normal # the '^C' indicator on a canceled command
 set -U fish_color_search_match normal # history search matches and selected pager items (background only)
 set -U fish_pager_color_progress normal # the progress bar at the bottom left corner
 set -U fish_pager_color_background --background=normal # the background color of a line
-set -U fish_pager_color_prefix WHITE --underline # the prefix string, i.e. the string that is to be completed
-set -U fish_pager_color_completion BRBLACK # the completion itself, i.e. the proposed rest of the string
-set -U fish_pager_color_description BRBLACK # the completion description
+set -U fish_pager_color_prefix b9b1bc --underline # the prefix string, i.e. the string that is to be completed
+set -U fish_pager_color_completion 7f7094 # the completion itself, i.e. the proposed rest of the string
+set -U fish_pager_color_description 7f7094 # the completion description
 set -U fish_pager_color_selected_background --background=normal # background of the selected completion
-set -U fish_pager_color_selected_prefix BRGREEN --bold --underline # prefix of the selected completion
-set -U fish_pager_color_selected_completion BRGREEN # suffix of the selected completion
-set -U fish_pager_color_selected_description BRWHITE # description of the selected completion
+set -U fish_pager_color_selected_prefix 0ae4a4 --bold --underline # prefix of the selected completion
+set -U fish_pager_color_selected_completion 0ae4a4 # suffix of the selected completion
+set -U fish_pager_color_selected_description f2f2e3 # description of the selected completion
 set -U fish_pager_color_secondary_background normal # background of every second unselected completion
-set -U fish_pager_color_secondary_prefix WHITE --underline # prefix of every second unselected completion
-set -U fish_pager_color_secondary_completion BRBLACK # suffix of every second unselected completion
-set -U fish_pager_color_secondary_description BRBLACK # description of every second unselected completion
+set -U fish_pager_color_secondary_prefix b9b1bc --underline # prefix of every second unselected completion
+set -U fish_pager_color_secondary_completion 7f7094 # suffix of every second unselected completion
+set -U fish_pager_color_secondary_description 7f7094 # description of every second unselected completion
 
 ```
 
--- tools/templates/fish.sh
+++ tools/templates/fish.sh
@@ -1,18 +1,18 @@
 # https://fishshell.com/docs/current/cmds/set_color.html#cmd-set-color
 
-set -U fish_color_normal WHITE # default color
-set -U fish_color_command BRMAGENTA # commands like echo
-set -U fish_color_keyword BRMAGENTA # keywords like if - this falls back on the command color if unset
-set -U fish_color_quote BRCYAN # quoted text like "abc"
-set -U fish_color_redirection BRYELLOW # IO redirections like >/dev/null
-set -U fish_color_end BRYELLOW # process separators like ';' and '&'
-set -U fish_color_error WHITE # syntax errors
-set -U fish_color_param BRWHITE # ordinary command parameters
-set -U fish_color_comment BRBLACK # comments like '# important'
+set -U fish_color_normal {WHITE_DARK.hex} # default color
+set -U fish_color_command {MAGENTA.hex} # commands like echo
+set -U fish_color_keyword {MAGENTA.hex} # keywords like if - this falls back on the command color if unset
+set -U fish_color_quote {CYAN.hex} # quoted text like "abc"
+set -U fish_color_redirection {YELLOW.hex} # IO redirections like >/dev/null
+set -U fish_color_end {YELLOW.hex} # process separators like ';' and '&'
+set -U fish_color_error {WHITE_DARK.hex} # syntax errors
+set -U fish_color_param {WHITE.hex} # ordinary command parameters
+set -U fish_color_comment {WHITE_DARKER.hex} # comments like '# important'
 set -U fish_color_selection normal # selected text in vi visual mode
-set -U fish_color_operator BRYELLOW # parameter expansion operators like '*' and '~'
-set -U fish_color_escape CYAN # character escapes like 'n' and 'x70'
-set -U fish_color_autosuggestion BRBLACK # autosuggestions (the proposed rest of a command)
+set -U fish_color_operator {YELLOW.hex} # parameter expansion operators like '*' and '~'
+set -U fish_color_escape {CYAN_DARK.hex} # character escapes like 'n' and 'x70'
+set -U fish_color_autosuggestion {WHITE_DARKER.hex} # autosuggestions (the proposed rest of a command)
 set -U fish_color_cwd normal # the current working directory in the default prompt
 set -U fish_color_user normal # the username in the default prompt
 set -U fish_color_host normal # the hostname in the default prompt
@@ -21,14 +21,14 @@ set -U fish_color_cancel normal # the '^C' indicator on a canceled command
 set -U fish_color_search_match normal # history search matches and selected pager items (background only)
 set -U fish_pager_color_progress normal # the progress bar at the bottom left corner
 set -U fish_pager_color_background --background=normal # the background color of a line
-set -U fish_pager_color_prefix WHITE --underline # the prefix string, i.e. the string that is to be completed
-set -U fish_pager_color_completion BRBLACK # the completion itself, i.e. the proposed rest of the string
-set -U fish_pager_color_description BRBLACK # the completion description
+set -U fish_pager_color_prefix {WHITE_DARK.hex} --underline # the prefix string, i.e. the string that is to be completed
+set -U fish_pager_color_completion {WHITE_DARKER.hex} # the completion itself, i.e. the proposed rest of the string
+set -U fish_pager_color_description {WHITE_DARKER.hex} # the completion description
 set -U fish_pager_color_selected_background --background=normal # background of the selected completion
-set -U fish_pager_color_selected_prefix BRGREEN --bold --underline # prefix of the selected completion
-set -U fish_pager_color_selected_completion BRGREEN # suffix of the selected completion
-set -U fish_pager_color_selected_description BRWHITE # description of the selected completion
+set -U fish_pager_color_selected_prefix {GREEN.hex} --bold --underline # prefix of the selected completion
+set -U fish_pager_color_selected_completion {GREEN.hex} # suffix of the selected completion
+set -U fish_pager_color_selected_description {WHITE.hex} # description of the selected completion
 set -U fish_pager_color_secondary_background normal # background of every second unselected completion
-set -U fish_pager_color_secondary_prefix WHITE --underline # prefix of every second unselected completion
-set -U fish_pager_color_secondary_completion BRBLACK # suffix of every second unselected completion
-set -U fish_pager_color_secondary_description BRBLACK # description of every second unselected completion
+set -U fish_pager_color_secondary_prefix {WHITE_DARK.hex} --underline # prefix of every second unselected completion
+set -U fish_pager_color_secondary_completion {WHITE_DARKER.hex} # suffix of every second unselected completion
+set -U fish_pager_color_secondary_description {WHITE_DARKER.hex} # description of every second unselected completion
